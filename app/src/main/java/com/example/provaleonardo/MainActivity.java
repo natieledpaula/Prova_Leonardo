be67bc0ed97091c00f81a7440e7ddbe491b3566d
Controle-De-Gastos <alanrossiniads@gmail.com>
2026-04-15 20:10:09 -0300

    Fazendo  parte dos botões e das Variaveis

--- app/src/main/java/com/example/provaleonardo/MainActivity.java
+++ app/src/main/java/com/example/provaleonardo/MainActivity.java
@@ -1,11 +1,42 @@
 package com.example.provaleonardo;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editTextName, editTextCpf, editTextEmail, editTextPhone;
-    private UserDao userDao;
+    private EditText editTextName, editTextCodigo, editTextPreco, editTextQuantidade;
+    private ProdutoDao produtoDao;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextCodigo = findViewById(R.id.editTextCodigo);
+        editTextPreco = findViewById(R.id.editTextPreco);
+        editTextQuantidade = findViewById(R.id.edittextQuantidade);
+
+        Button btnSalvar = findViewById(R.id.btnSalvar);
+        Button btnLista = findViewById(R.id.btnLista);
+
+        ProdutoDatabase db = Room.databaseBuilder(
+                getApplication(), ProdutoDatabase.class, "produto-db").allowMainThreadQueries().build();
+
+        produtoDao = db.produtoDao();
+
+        btnSalvar.setOnClickListener( v -> {
+            Log.d("MainActivity", "Botão salvar clicando");
+
+            String nome = editTextName.getText().toString().trim();
+            String codigo = editTextCodigo.getText().toString().trim();
+            String preco = editTextPreco.getText().toString().trim();
+            String quatidade = editTextQuantidade.getText().toString().trim();
+        });
+    }
 
 }
