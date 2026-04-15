67c63089ad1880f68173687d4289291740d34f08
Controle-De-Gastos <alanrossiniads@gmail.com>
2026-04-15 19:30:25 -0300

    Fazendo a parte dos produtos

---
+++ app/src/main/java/com/example/provaleonardo/Produto.java
@@ -0,0 +1,14 @@
+package com.example.provaleonardo;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Produto {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String nome;
+    public String codigo;
+    public double preco;
+    public int quantidade;
+}
---
+++ app/src/main/java/com/example/provaleonardo/ProdutoDao.java
@@ -0,0 +1,8 @@
+package com.example.provaleonardo;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+@Dao
+public class ProdutoDao {
+}
